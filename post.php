67c561486f3034f56d5a61a4f9ef6fb0b030ed55
milan-kharel <milan.kharel120@gmail.com>
2022-11-07 04:06:57 +0545

    :sparkle:Updated Product Section

--- post.php
+++ post.php
@@ -6,21 +6,40 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap');
+
         a {
-            color: #fff;
+            color: black;
         }
 
         * {
-            font-family: Arial, Helvetica, sans-serif;
+            font-family: 'Poppins', sans-serif;
             text-decoration: none;
             color: #FCE2DB;
             font-size: 1.05rem;
         }
+
+        .price-type{
+            display: flex;
+            line-height: -0.2em;
+        }
+
+        .price-type div{
+            margin: 0 0.5em;
+            padding: 0;
+        }
+
+        .price-type :nth-child(3):hover{
+            border-bottom: 2px dotted #ad0000 ;
+            /* border-top: #ad0000;
+            border-top: #ad0000; */
+        }
+
     </style>
     <title>Bidyarthisewa</title>
 </head>
 
-<body style="background-color:#80489C ;">
+<body style="background-color:white;">
 
     <?php
 
@@ -41,9 +60,9 @@
         $usrid = $row['user_id'];
         echo '<br>
        
-            <div class="cont" style="min-height:80vh;display:flex; flex-direction:column;justify-content:space-around;align-items:center;">
+            <div class="cont" style="min-height:80vh;display:flex; flex-direction:column;justify-content:space-around;align-items:center;background-color:#DCBEBE;">
 
-                <div><h1 class="name"><b style="font-size:2rem; text-transform:uppercase;">' . $bok_n . '</b></h1></div>
+                <div><h1 class="name"><b style="font-size:2rem; text-transform:uppercase;color:black;">' . $bok_n . '</b></h1></div>
     
                 <div class="pic-cont" style=" width:45rem;height:17rem;display:flex; justify-content:center;align-items:center;gap:0.3rem;" >
                     <div class="pic">
@@ -56,29 +75,28 @@
                         <img src="project_x_copy/' . $pic3 . '" style="height:18rem;width:15rem">
                     </div>
                 </div>
-                <p class="lead">
-                <h6> Description </h6>
+                <div class="lead" style="color:black;">
+                <h6 style="color:black;text-align:center"> Description </h6>
                 ' . $des_n . '
-                </p>
+                </div>
               
                 <!--row2-->
-                <div class="">
-                    <div class="">
-                        <div class="">
-                            <h6>Original Price: </h6> ' . $og_pr . '
+                <div class="price-type" >
+                    <div class="" >
+                        <div class="" style="color:black;text-align:center;">
+                            <h6 style="color:black;">Original Price: </h6> ' . $og_pr . '
                         </div>
                     </div>
                     <div class="">
-                        <div class="">
-                            <h6>Expected Price: </h6> ' . $ex_pr . '
+                        <div class="" style="color:#ad0000;text-align:center;">
+                            <h6 style="color:black;">Expected Price: </h6> ' . $ex_pr . '
                         </div>
+                    </div>
+                    <div>
+                        <a href="partials/buyrequest.php?pid=' . $p_id . '&owid=' . $usrid . '" style="border:2px solid black;font-size:1rem;font-weight:900" class="buy-request-btn">Request to Buy</a>
                     </div>
                 </div>
               
-            </div>
-
-            <div>
-            <a href="partials/buyrequest.php?pid=' . $p_id . '&owid=' . $usrid . '" style=" padding:5px;border:1px solid white;text-align:center;font-size:1.5rem;margin-left:45%">Request to Buy</a>
             </div>
      
        
